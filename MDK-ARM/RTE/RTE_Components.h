cedcfa124a8f827c7fe857f8b00705fd15266016
Dam Duy Hung <hungdamduy@gmail.com>
2017-03-24 06:42:06 +0700

    Initial commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HIENTHILCD' 
+ * Target:  'HIENTHILCD' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
